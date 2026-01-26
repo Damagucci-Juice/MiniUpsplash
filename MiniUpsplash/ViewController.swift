590d3b66f9a0b68d0fb1624c6e40b9a39dcc8291
Damagucci-Juice <jsr9874@gmail.com>
2026-01-26 18:30:56 +0900

    init project

---
+++ MiniUpsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniUpsplash
+//
+//  Created by Gucci on 1/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
