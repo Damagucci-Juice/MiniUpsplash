78b9aa054c44ef0f89acb35046a140759d3c234a
Damagucci-Juice <jsr9874@gmail.com>
2026-01-27 20:36:46 +0900

    feat: TabBar 로 코드 시작

--- MiniUpsplash/App/SceneDelegate.swift
+++ MiniUpsplash/App/SceneDelegate.swift
@@ -15,8 +15,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let nav = UINavigationController(rootViewController: TopicViewController(service: MockAPIService.shared))
-        self.window?.rootViewController = nav
+        let service = MockAPIService.shared
+        let topicNav = UINavigationController(rootViewController: TopicViewController(service: service))
+        let searchNav = UINavigationController(rootViewController: SearchViewController(service: service))
+
+        topicNav.tabBarItem = UITabBarItem(title: "토픽", image: UIImage(systemName: "laser.burst"), selectedImage: nil)
+        searchNav.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "sparkle.magnifyingglass"), selectedImage: nil)
+
+        let tab = UITabBarController()
+        tab.viewControllers = [topicNav, searchNav]
+        self.window?.rootViewController = tab
         self.window?.makeKeyAndVisible()
     }
 
