c007d67ce3e743c4aa9c9a7cc41690f2f8434a8f
Damagucci-Juice <jsr9874@gmail.com>
2026-01-29 08:59:45 +0900

    feat: chart 영역 어노테이션 구현

--- MiniUpsplash/Presentation/Detail/ChartView.swift
+++ MiniUpsplash/Presentation/Detail/ChartView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 import Charts
 
 struct ChartView: View {
+    @State private var selectedDate: String?
     let historical: [ValueInfo]
+
     var body: some View {
         Chart(historical) { value in
             AreaMark(
@@ -23,9 +25,49 @@ struct ChartView: View {
                     endPoint: .bottom
                 )
             )
+
+            if let selectedDate,
+               let value = historical.first(where: { value in
+                    value.date == selectedDate
+                })
+            {
+                RectangleMark(x: .value("Day", selectedDate))
+                    .foregroundStyle(.primary.opacity(0.2))
+                    .annotation(
+                        position: .leading,
+                        alignment: .center, spacing: 0
+                    ) {
+                        TrendAnnotationView(values: historical, date: selectedDate, valueData: value.value)
+                    }
+                    .accessibilityHidden(true)
+            }
         }
         .chartXAxis(.hidden)
         .chartYAxis(.hidden)
+        .chartOverlay { (chartProxy: ChartProxy) in
+            GeometryReader { geometry in
+                Rectangle()
+                    .fill(Color.clear)
+                    .contentShape(Rectangle()) // 터치 영역 확보
+                    .gesture(
+                        DragGesture(minimumDistance: 0) // 터치 시작부터 감지
+                            .onChanged { value in
+                                let origin = geometry[chartProxy.plotAreaFrame].origin
+                                let location = CGPoint(
+                                    x: value.location.x - origin.x,
+                                    y: value.location.y - origin.y
+                                )
+                                // x축 위치에 해당하는 날짜 값 추출
+                                if let date: String = chartProxy.value(atX: location.x) {
+                                    selectedDate = date
+                                }
+                            }
+                            .onEnded { _ in
+                                selectedDate = nil // 손을 떼면 어노테이션 제거
+                            }
+                    )
+            }
+        }
     }
 }
 
@@ -40,3 +82,32 @@ struct ChartView: View {
     ChartView(historical: mockData)
 }
 
+
+struct TrendAnnotationView: View {
+    let values: [ValueInfo]
+    let date: String
+    let valueData: Int
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(date)
+                .font(.headline)
+            Divider()
+            ForEach(values, id: \.self.id) { origin in
+                Text("\(origin.date): \(NumberManager.shared.convert(origin.value))")
+            }
+        }
+        .padding()
+        .background(Color.annotationBackground)
+    }
+}
+
+extension Color {
+    static var annotationBackground: Color {
+        #if os(macOS)
+        return Color(nsColor: .controlBackgroundColor)
+        #else
+        return Color(uiColor: .secondarySystemBackground)
+        #endif
+    }
+}
